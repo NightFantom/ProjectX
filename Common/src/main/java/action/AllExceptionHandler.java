5dea950207000d9905d6d2867411455513cfeffd
Denis <Denis@mail.ru>
2015-01-02 17:38:54 +0300

    Добавление логирования. Часть 1

--- Common/src/main/java/action/AllExceptionHandler.java
+++ Common/src/main/java/action/AllExceptionHandler.java
@@ -5,6 +5,8 @@
  */
 package action;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -16,10 +18,12 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AllExceptionHandler extends ExceptionHandler {
 
+    protected static final Logger LOG = LogManager.getLogger(AllExceptionHandler.class);
+
     @Override
     public ActionForward execute(Exception ex, ExceptionConfig ae, ActionMapping mapping, ActionForm formInstance, HttpServletRequest request, HttpServletResponse response) throws ServletException {
         ActionForward forward = super.execute(ex, ae, mapping, formInstance, request, response);
-        ex.printStackTrace();
+        LOG.error("Ошибка в Action",ex);
         forward.setRedirect(true);
         return forward;
     }
--- Common/src/main/java/action/LogDispatchAction.java
+++ Common/src/main/java/action/LogDispatchAction.java
@@ -5,6 +5,8 @@
 package action;
 
 import helpers.GlobalConstants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -17,6 +19,7 @@ public class LogDispatchAction extends DispatchAction {
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         //Запоминаем текущую форму
+        log.debug("Форма занесена в request");
         request.setAttribute(GlobalConstants.CURRENT_ACTION_FORM, form);
         return super.execute(mapping, form, request, response);
     }
