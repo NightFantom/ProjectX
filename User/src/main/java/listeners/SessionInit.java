aa9a0cfb0c14af6b8f8d8a519be890b23aae7298
VictorCat <mastersavage@mail.ru>
2015-01-31 23:37:56 +0300

    Signed-off-by: VictorCat <mastersavage@mail.ru>

--- User/src/main/java/listeners/SessionInit.java
+++ User/src/main/java/listeners/SessionInit.java
@@ -12,8 +12,8 @@ import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 public class SessionInit implements HttpSessionListener {
-
-    private static final City DEFAULT_CITY = new City("Вологда", 1);
+    // TODO id города 3!!
+    private static final City DEFAULT_CITY = new City("Вологда", 3);
 
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         httpSessionEvent.getSession().setAttribute(GlobalConstants.ID_USER_CITY, DEFAULT_CITY);
