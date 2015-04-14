ada74b81a01996cdac8d2ce88d72bce80126e3d6
VictorCat <mastersavage@mail.ru>
2015-04-15 00:20:44 +0300

    Склонение слова "Предложение" от числительного

--- Common/src/main/java/helpers/StringHelper.java
+++ Common/src/main/java/helpers/StringHelper.java
@@ -34,4 +34,25 @@ public class StringHelper {
     public static String[] slit(String str, String delimiter){
         return str.split(delimiter);
     }
+
+    /**
+     * Склоняет слово "Предложение" от числительного
+     * @param number - числительное в диапозоне от 0 до 99
+     * @return "Предложение в нужном падеже"
+     */
+    public static String declension(Long number){
+
+        if(11<=number && number<=14){
+            return "Предложений";
+        }else{
+            long rest =number % 10;
+            if(rest==1){
+                return "Предложение";
+            }else if(2<=rest && rest<=4){
+                return "Предложения";
+            }else {
+                return "Предложений";
+            }
+        }
+    }
 }
